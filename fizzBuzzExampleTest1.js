4c39eb190e6b617529b8799538a8202e09c1edd6
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2022-08-06 17:01:48 -0400

    corre con errores

--- fizzBuzzExampleTest1.js
+++ fizzBuzzExampleTest1.js
@@ -1,13 +1,21 @@
 function fizzBuzz(n) {
     // Write your code here
 
-    for (var i = 0; i <= n; i++){
+    for (var i = 0; i <= n; i++) {
         if (i % 3 && i % 5) {
             console.log('FizzBuzz');
 
+        }else if (i % 3) {
+            console.log('Fizz');
+
+        }else if (i % 5) {
+            console.log('Buzz');
+
+        }else {
+            console.log(i);
+        }
+
+
+
     }
-
-
-
-}
 }
